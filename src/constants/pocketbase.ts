2f5b2d6f0ce6d758b84630b16ab45006d86b9902
Manassarn Manoonchai <manassarn@gmail.com>
2023-05-09 21:27:36 +0700

    Load event with id

--- src/constants/pocketbase.ts
+++ src/constants/pocketbase.ts
@@ -1,3 +1,8 @@
 import PocketBase from 'pocketbase'
 
 export const pocketbase = new PocketBase(import.meta.env.POCKETBASE_URL)
+
+await pocketbase.admins.authWithPassword(
+  import.meta.env.PUBLIC_POCKETBASE_ADMIN_USERNAME,
+  import.meta.env.PUBLIC_POCKETBASE_ADMIN_PASSWORD
+)
